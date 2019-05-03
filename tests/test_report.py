9f2befeecd585ca19a29006f1bc597d54a5a0af5
Rajat Mhetre <mhetrerajat@gmail.com>
2019-05-04 00:59:51 +0530

    add test cases for csv report

--- tests/test_report.py
+++ tests/test_report.py
@@ -2,12 +2,18 @@ import json
 
 from app.utils.url_builder import URLBuilder
 from tests.base import BaseTestCase
+import re
 
 url_builder = URLBuilder()
 
 
 def get_report(self, params):
     url = url_builder.build('/api/v1/report/', params=params)
+    return self.client.get(url)
+
+
+def get_csv_report(self, params):
+    url = url_builder.build('/api/v1/report/csv', params=params)
     return self.client.get(url)
 
 
@@ -68,3 +74,17 @@ class ReportTestCases(BaseTestCase):
         data = json.loads(response.data.decode())
 
         self.assert400(response)
+
+    def test_csv_report(self):
+        params = {
+            'group_by': 'year',
+            'group_by': 'agency',
+            'start_year': '2005',
+            'end_year': '2007'
+        }
+        response = get_csv_report(self, params)
+        content = response.headers["Content-Disposition"]
+        _pattern = re.search('.csv$', content)
+        extension = _pattern.group(0) if _pattern else None
+        self.assertEqual(extension, '.csv')
+        self.assert200(response)
