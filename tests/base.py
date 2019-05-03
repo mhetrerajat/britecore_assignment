5c3a95764221702d11d17ef0bc6649984024f2b0
Rajat Mhetre <mhetrerajat@gmail.com>
2019-05-03 10:53:07 +0530

    initialize test environment with dummy data

--- tests/base.py
+++ tests/base.py
@@ -1,7 +1,11 @@
+import os
+
 from flask_testing import TestCase
 
 from app import create_app as _create_app
 from app import db
+from app.utils.parser import DataParser
+from config import BASE_DIR
 
 
 class BaseTestCase(TestCase):
@@ -14,6 +18,11 @@ class BaseTestCase(TestCase):
     def setUp(self):
         db.create_all()
         db.session.commit()
+
+        # Add dummy data
+        path = os.path.join(BASE_DIR, "sample_data.csv")
+        p = DataParser(path, sample_size=0.5, query={'AGENCY_ID': '3'})
+        p.parse()
 
     def tearDown(self):
         db.session.remove()
